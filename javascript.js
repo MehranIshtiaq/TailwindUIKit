1d4f6032a1b5d2501c449d2912bfa68a3395b075
Mehran Ishtiaq <80805794+MehranIshtiaq@users.noreply.github.com>
2023-08-24 19:46:08 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+
+function myFunction(){
+    var z = document.getElementById("mobileMenu");
+
+    z.style.display = "none";
+}
+
+function myFunction2(){
+    var a = document.getElementById("mobileMenu");
+
+    a.style.display = "block";
+}
